7baeb1217bc547b46eaee53ee1f9a4e65078911b
Ddubby <league@WorthyMini-5.attlocal.net>
2019-03-25 16:38:03 -0700

    Done

--- src/_01_introduction/ExceptionsDemo.java
+++ src/_01_introduction/ExceptionsDemo.java
@@ -30,6 +30,24 @@ public class ExceptionsDemo {
 		//    CustomException object.
 		//    This demonstrates how custom exception classes can be used to
 		//    handle exceptions in unlimited ways.
+		try {
+			testMethod1(4);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error");
+		}
+		try {
+			testMethod1(6);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error");
+		}
+		try {
+			testMethod2(-2);
+		} catch (CustomException e) {
+			// TODO Auto-generated catch block
+			e.terminate();
+		}
 	}
 	
 	public static void testMethod1(int x) throws Exception {
@@ -40,4 +58,9 @@ public class ExceptionsDemo {
 	
 	// 4. Create a static void method called testMethod2(int x) that throws
 	//    a CustomException if x is negative.
+	public static void testMethod2(int x) throws CustomException {
+		if (x < 0) {
+			throw new CustomException();
+		}
+	}
 }
--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,21 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
+	public double divide(double x, double y) throws IllegalArgumentException {
+		if (y == 0.0) {
+			throw new IllegalArgumentException();
+		}
+		return x/y;
+	}
 	
+	public String reverseString(String s) throws IllegalStateException{
+		String s2 = "";
+		if (s.length() == 0) {
+			throw new IllegalStateException();
+		}
+		for (int i = s.length(); i > 0; i--) {
+			s2 += s.substring(i - 1,i);
+		}
+		return s2;
+	}
 }
